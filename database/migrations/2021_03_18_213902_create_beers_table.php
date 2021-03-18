62fd0de05ff77ce847f808f5b68e234d3651c53e
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-18 23:02:12 +0100

    creato db Beers + tabella, migration, model, controllerBeer

---
+++ database/migrations/2021_03_18_213902_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('type', 40);
+            $table->float('quantityL', 5, 2);
+            $table->float('price', 6, 2);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', 'ControllerBeer');
+
+
+
+// Route::get('/', function () {
+//     return view('home');
+// });
